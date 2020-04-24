6ab97d094a0593a3c853b7199ae15cbd9daa4c6b
caozhen <wuwangchuxincz@163.com>
2020-04-24 14:26:03 +0800

    day04实例完成

--- day04/Comment/src/App.vue
+++ day04/Comment/src/App.vue
@@ -12,8 +12,8 @@
 
     <div class="container">
       <!-- 3. 使用组件 -->
-      <AddComment />
-      <CommentList />
+      <AddComment :addComment="addComment" />
+      <CommentList :comments="comments" :delComment="delComment" />
     </div>
   </div>
 </template>
@@ -24,6 +24,22 @@ import AddComment from "@comps/AddComment";
 import CommentList from "@comps/CommentList";
 
 export default {
+  data() {
+    return {
+      comments: [
+        { id: 1, name: "cz", content: "vue" },
+        { id: 2, name: "caozhen", content: "vue1" },
+      ],
+    };
+  },
+  methods: {
+    addComment(comment) {
+      this.comments.unshift(comment);
+    },
+    delComment(id) {
+      this.comments = this.comments.filter((comment) => comment.id !== id);
+    },
+  },
   components: {
     // 2. 注册组件
     AddComment,
